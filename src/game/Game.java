1082f7c03d2e2f427959137f16fb87a8c21f62f7
Your Name <krasava1081@gmail.com>
2021-09-21 15:38:26 +0300

    Third Update

--- src/game/Game.java
+++ src/game/Game.java
@@ -18,7 +18,9 @@ public class Game {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int max = 5, min =0, attempt=1;
+        
+        int max = 5, min =0, attempt=1, ii=1;
+        while(ii==1){
         Random random= new Random();
         int gameNum = random.nextInt(max - min + 1)+min;
         System.out.println("Программа задала число от 0 до 5, угадай:"); 
@@ -27,17 +29,29 @@ public class Game {
             int userNum= scanner.nextInt(); 
             if(gameNum==userNum){
                 System.out.println("Ура! Ты выйграл!");
+                ii=ii-1;
                 break;
             }else{
                 if(attempt < 3){
                     System.out.println("Неправильно, попробуй еще раз:");
                 }else{
                     System.out.println("Ты проиграл... Задумано число:" + gameNum);
-                    break;
-                   
+                    System.out.println("Хотите сыграть еще раз? Если да наберите 1, если нет любое другое число:");
+                    int choice= scanner.nextInt();
+                    if(choice==1){
+                        attempt=1;
+                        break;
+                    }else{
+                        ii=ii-1; 
+                        System.out.println("Пока!");
+                         break;
+                    }
+                
             }
         } 
-        attempt++;        
+        attempt++;
+        
+    }
     }
     }
 }
